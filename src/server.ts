2cb176c0e9e68bc3a90bf4ba4f651135caeb5ba9
Eduardo Bravo <eduardobravopires@gmail.com>
2021-06-22 13:44:11 -0300

    add typeorm

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,8 @@
+import 'reflect-metadata'
 import express from 'express'
+import './database'
 
 const app = express()
-
-app.get('/test', (request, response) => {
-  return response.send("Olá NLW")
-})
-
 
 app.listen(3333, () => {
   console.log('🔥 Server is running on port 3333')
