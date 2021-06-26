c3e14ae74107f6317281ddda35f6e9768b6839ec
Eduardo Bravo <eduardobravopires@gmail.com>
2021-06-26 19:54:01 -0300

    created list tags, compliments and middleware

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,11 @@ import { AuthenticateUserController } from "./controllers/AuthenticateUserContro
 import { CreateComplimentController } from "./controllers/CreateComplimentController";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { CreateUserController } from "./controllers/CreateUserController";
+import { ListTagsController } from "./controllers/ListTagsController";
+import { ListUserReceiveComplimentsController } from "./controllers/ListUserReceiveComplimentsController";
+import { ListUserSendComplimentsController } from "./controllers/ListUserSendComplimentsController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
+import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
 
 const router = Router()
 
@@ -11,10 +15,22 @@ const createUserController = new CreateUserController()
 const createTagController = new CreateTagController()
 const authenticateUserController = new AuthenticateUserController()
 const createComplimentController = new CreateComplimentController()
+const listUserReceiveComplimentsController = new ListUserReceiveComplimentsController()
+const listUserSendComplimentsController = new ListUserSendComplimentsController()
+const listTagsController = new ListTagsController()
 
 router.post('/users', createUserController.handle)
-router.post('/tags', ensureAdmin, createTagController.handle)
+
+router.post('/tags', ensureAuthenticated, ensureAdmin, createTagController.handle)
+
 router.post('/sessions', authenticateUserController.handle)
-router.post('/compliments', createComplimentController.handle)
+
+router.post('/compliments', ensureAuthenticated, createComplimentController.handle)
+
+router.get('/users/compliments/send', ensureAuthenticated, listUserSendComplimentsController.handle)
+
+router.get('/users/compliments/receive', ensureAuthenticated, listUserReceiveComplimentsController.handle)
+
+router.get('/tags', ensureAuthenticated, listTagsController.handle)
 
 export { router }
